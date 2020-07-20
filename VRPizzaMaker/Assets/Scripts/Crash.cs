ba3eeb6e933a65a140d3b300ae93783a3d3a9e6c
kohemi <gostone11@gmail.com>
2020-07-20 14:57:05 +0900

    1인칭 시점 플레이어 및 물건 집기 상호작용 기능 구현

---
+++ VRPizzaMaker/Assets/Scripts/Crash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crash : MonoBehaviour
+{
+    public GameObject egg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
