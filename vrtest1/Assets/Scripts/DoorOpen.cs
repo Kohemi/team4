eb93765d63fe67b04498496231fc4085fcf91b53
Hemi <gostone11@gmail.com>
2020-07-29 16:50:50 +0900

    문 열기 스크립트 추가

---
+++ vrtest1/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "MainCharacterHand")
+        {
+            Debug.Log("Collision");
+            transform.GetComponent<Animator>().SetBool("isOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "MainCharacterHand")
+        {
+            Debug.Log("NoCollision");
+            transform.GetComponent<Animator>().SetBool("isOpen", false);
+        }
+    }
+}
---
+++ vrtest1/Assets/Scripts/Touching.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCasting : MonoBehaviour
+{
+    public GameObject pCylinder1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dough = GameObject.Find("")
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "MainCharacterHand")
+        {
+            transform
+        }
+        
+    }
+}
